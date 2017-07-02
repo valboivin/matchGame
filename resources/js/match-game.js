e1d73dbbb1ac323125edebd67d6a274eb6b03da5
Valerie Boivin <val.boivin@hotmail.com>
2017-07-02 19:40:09 -0400

    Create JS method to randomize card numbers and Flip Cards

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -1,59 +1,32 @@
 var MatchGame = {};
-
 /*
   Sets up a new game after HTML document has loaded.
   Renders a 4x4 board of cards.
 */
 
+$(document).ready (function(){
+  MatchGame.renderCards(MatchGame.generateCardValues(), $('#game'));
+});
+
+
+
 /*
   Generates and returns an array of matching card values.
  */
 
-
-/*
-
-var cardValues = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
-
-
-cardValues.sort(function(){
-  return 0.5- Math.random();
-});
-
-for (var cardShuffled= 0; cardShuffled < cardValues.length; cardShuffled++){
- console.log(cardValues[cardShuffled]);
-}
-
-*/
-
- var cardValues = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
-
-
- cardValues.sort(function(){
-   return 0.5- Math.random();
- });
-
-for (var cardShuffled= 0; cardShuffled < cardValues.length; cardShuffled++){
-  console.log(cardValues[cardShuffled]);
-}
-
-
-
-
 MatchGame.generateCardValues = function () {
-var shuffledValues = [];
-for (var i = 0; i < cardValues.Length; i++)
+  var cardValues = [];
+  for (var i = 1; i <= 8; i++) {
+    cardValues.push(i);
+    cardValues.push(i);
+    };
 
-};
+  cardValues.sort(function(){
+    return 0.5- Math.random();
+  });
 
-
-/*
-
-var condiments = ['Ketchup', 'Mustard', 'Sauerkraut'];
-var condimentsLength = condiments.length;
-for (var i = 0; i < condimentsLength; i++) {
-  console.log('Condiment: ' + condiments[i]);
+  return cardValues;
 }
-*/
 
 
 /*
@@ -62,7 +35,31 @@ for (var i = 0; i < condimentsLength; i++) {
 */
 
 MatchGame.renderCards = function(cardValues, $game) {
-$game.empty();
+  var cardColors = [
+    'hsl(25,85%, 65%)',
+    'hsl(55,85%, 65%)',
+    'hsl(90,85%, 65%)',
+    'hsl(160,85%, 65%)',
+    'hsl(220,85%, 65%)',
+    'hsl(265,85%, 65%)',
+    'hsl(310,85%, 65%)',
+    'hsl(360,85%, 65%)'];
+
+  $game.data ('flippedCards', [] );
+
+  $game.empty();
+
+  for (var i = 0; i < cardValues.length; i++) {
+    var $card = $('<div class="card col-md-3 col-xs-3"></div>');
+    $card.data('value', cardValues[i]);
+    $card.attr('data-flipped', 'false');
+    $card.data('color', cardColors[cardValues[i]-1]);
+
+    $game.append($card);
+    $card.on('click', function(){
+      MatchGame.flipCard($(this), $game);
+    });
+  }
 };
 
 /*
@@ -71,5 +68,31 @@ $game.empty();
  */
 
 MatchGame.flipCard = function($card, $game) {
+  var flippedCards = $game.data('flippedCards');
 
+  // if (flippedCards.indexOf($card.data('value')) == -1) {
+    $card.css('background-color', $card.data('color'));
+    $card.text($card.data('value'));
+    $card.attr('data-flipped', 'true');
+    flippedCards.push($card.data('value'));
+
+    if (flippedCards.length == 2 ) {
+      if (flippedCards[0] == flippedCards[1]) {
+        $('data-flipped["true"]').css('background-color', 'rgb(153,153,153)');
+        $('data-flipped["true"]').css('color', 'rgb(204,204,204)');
+      } else {
+        $('.card').css('background-color', '');
+        $('.card').text('');
+        $('.card').attr('data-flipped', 'false');
+      }
+      $game.data ('flippedCards', []);
+    } else {
+
+    }
+
+  // }
+
+  // else {
+    // return false;
+  // };
 };
